398d8776d17b37d0f4daf11751d316234751134d
nkhaccuong <cuong19012002@gmail.com>
2024-09-30 16:43:36 +0700

    Router DOM

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -3,15 +3,19 @@ const TodoData = (props) => {
 
     //destructuring
     // const { name, age, data } = props;
-    const { todoList } = props;
+    const { todoList, deleteTodo } = props;
     console.log("Check props: ", todoList);
+    const handleClick = (id) => {
+        deleteTodo(id);
+    }
+
     return (
         <div className="todo-data">
             {todoList.map((item, index) => {
                 return (
                     <div className="todo-item" key={item.id}>
                         <div>{item.name} </div>
-                        <button>Delete</button>
+                        <button onClick={() => handleClick(item.id)}>Delete</button>
                     </div>
                 )
             })}
